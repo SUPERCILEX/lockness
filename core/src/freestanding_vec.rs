5a94af77e4f8a4175ee4e02138c4c86ebb2e160a
Alex Saveau <saveau.alexandre@gmail.com>
2023-08-14 21:28:07 -0400

    Add generalization of ClosureVec
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ core/src/freestanding_vec.rs
@@ -0,0 +1,443 @@
+use std::{
+    alloc::{alloc, dealloc, handle_alloc_error, realloc, Layout},
+    cmp,
+    mem::{align_of, size_of, ManuallyDrop},
+    ptr,
+    ptr::NonNull,
+};
+
+struct Header<M> {
+    metadata: M,
+    value_size: usize,
+    alignment: usize,
+    len: usize,
+    capacity: usize,
+}
+
+impl<M> Header<M> {
+    unsafe fn from_ref<T>(value: &'_ FreestandingVec<M, T>) -> &'_ Self {
+        debug_assert!(value.is_allocated());
+        unsafe { &*value.data.cast::<Self>() }
+    }
+
+    unsafe fn from_mut<T>(value: &'_ mut FreestandingVec<M, T>) -> &'_ mut Self {
+        debug_assert!(value.is_allocated());
+        unsafe { &mut *value.data.cast::<Self>() }
+    }
+}
+
+union FirstEntry<M, T> {
+    _header: ManuallyDrop<Header<M>>,
+    _t: ManuallyDrop<T>, // Used to ensure proper alignment
+}
+
+pub struct FreestandingVec<M, T> {
+    data: *mut FirstEntry<M, T>,
+}
+
+impl<M, T> FreestandingVec<M, T> {
+    const MIN_NON_ZERO_CAP: usize = if size_of::<T>() == 1 {
+        8
+    } else if size_of::<T>() <= 1024 {
+        4
+    } else {
+        1
+    };
+
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            data: ptr::null_mut(),
+        }
+    }
+
+    fn deallocate(&mut self) {
+        if !self.is_allocated() {
+            return;
+        }
+
+        let &mut Header {
+            ref mut metadata,
+            value_size: _,
+            alignment,
+            len: _,
+            capacity: _,
+        } = unsafe { Header::from_mut(self) };
+
+        let _metadata_for_drop = unsafe { ptr::from_mut(metadata).read() };
+        unsafe {
+            let layout = Layout::from_size_align_unchecked(self.capacity_bytes(), alignment);
+            dealloc(self.data.cast(), layout);
+        }
+        self.data = ptr::null_mut();
+    }
+
+    fn is_allocated(&self) -> bool {
+        !self.data.is_null()
+    }
+
+    fn as_mut_ptr(&mut self) -> *mut T {
+        unsafe { self.data.add(1) }.cast::<T>()
+    }
+
+    #[must_use]
+    pub fn into_raw(self) -> *mut () {
+        let this = ManuallyDrop::new(self);
+        this.data.cast()
+    }
+
+    pub const unsafe fn from_raw(ptr: *mut ()) -> Self {
+        Self { data: ptr.cast() }
+    }
+
+    #[must_use]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    fn len(&self) -> usize {
+        if !self.is_allocated() {
+            return 0;
+        }
+
+        unsafe { Header::from_ref(self) }.len
+    }
+
+    fn capacity(&self) -> usize {
+        if !self.is_allocated() {
+            return 0;
+        }
+
+        unsafe { Header::from_ref(self) }.capacity
+    }
+
+    fn capacity_bytes(&self) -> usize {
+        if !self.is_allocated() {
+            return 0;
+        }
+
+        let Header {
+            metadata: _,
+            value_size,
+            alignment: _,
+            len: _,
+            capacity,
+        } = unsafe { Header::from_ref(self) };
+        size_of::<FirstEntry<M, T>>() + value_size * capacity
+    }
+
+    fn reserve(&mut self, additional: usize) -> usize {
+        let layout;
+        let new_capacity;
+
+        let ptr = if self.is_allocated() {
+            if size_of::<T>() == 0 {
+                return 0;
+            }
+
+            let required_cap = self.len().checked_add(additional).unwrap();
+
+            let og_cap = self.capacity();
+            if og_cap >= required_cap {
+                return og_cap;
+            }
+
+            new_capacity = cmp::max(og_cap * 2, required_cap);
+            layout = unsafe {
+                Layout::from_size_align_unchecked(
+                    self.capacity_bytes(),
+                    align_of::<FirstEntry<M, T>>(),
+                )
+            };
+
+            unsafe {
+                realloc(
+                    self.data.cast(),
+                    layout,
+                    size_of::<T>()
+                        .checked_mul(new_capacity)
+                        .and_then(|bytes| size_of::<FirstEntry<M, T>>().checked_add(bytes))
+                        .unwrap(),
+                )
+            }
+        } else {
+            new_capacity = if size_of::<T>() == 0 { 0 } else { additional };
+            layout = unsafe {
+                Layout::from_size_align_unchecked(
+                    size_of::<T>()
+                        .checked_mul(new_capacity)
+                        .and_then(|bytes| size_of::<FirstEntry<M, T>>().checked_add(bytes))
+                        .unwrap(),
+                    align_of::<FirstEntry<M, T>>(),
+                )
+            };
+
+            unsafe { alloc(layout) }
+        };
+
+        if ptr.is_null() {
+            handle_alloc_error(layout);
+        } else {
+            self.data = ptr.cast();
+        }
+
+        new_capacity
+    }
+
+    fn maybe_init(&mut self, init: impl FnOnce() -> M) {
+        if !self.is_empty() {
+            return;
+        }
+        let metadata = init();
+
+        if !self.data.is_aligned() {
+            self.deallocate();
+        }
+        let was_allocated = self.is_allocated();
+
+        let capacity = self.reserve(Self::MIN_NON_ZERO_CAP);
+        let ptr = self.data.cast::<Header<M>>();
+        let header = Header {
+            metadata,
+            value_size: size_of::<T>(),
+            alignment: align_of::<FirstEntry<M, T>>(),
+            len: 0,
+            capacity,
+        };
+        if was_allocated {
+            unsafe {
+                *ptr = header;
+            }
+        } else {
+            unsafe {
+                ptr.write(header);
+            }
+        }
+    }
+
+    pub fn push(&mut self, value: T, init: impl FnOnce() -> M) {
+        self.maybe_init(init);
+        let new_cap = self.reserve(1);
+
+        unsafe {
+            self.as_mut_ptr().add(self.len()).write(value);
+        }
+
+        let Header {
+            metadata: _,
+            value_size: _,
+            alignment: _,
+            len,
+            capacity,
+        } = unsafe { Header::from_mut(self) };
+        *len += 1;
+        *capacity = new_cap;
+    }
+
+    pub fn pop<O, F: FnOnce(&mut M, NonNull<T>) -> O>(&mut self, f: F) -> Option<O> {
+        let len = self.len();
+        if len == 0 {
+            return None;
+        }
+        let len = len - 1;
+
+        let ptr = self.as_mut_ptr().cast::<u8>();
+        let &mut Header {
+            ref mut metadata,
+            value_size,
+            alignment: _,
+            len: ref mut header_len,
+            capacity: _,
+        } = unsafe { Header::from_mut(self) };
+        let ptr = unsafe { NonNull::new_unchecked(ptr.add(len * value_size)) };
+
+        *header_len = len;
+        Some(f(metadata, ptr.cast()))
+    }
+}
+
+impl<M, T> Drop for FreestandingVec<M, T> {
+    fn drop(&mut self) {
+        self.deallocate();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::Cell, mem::transmute, rc::Rc};
+
+    use super::*;
+
+    #[test]
+    fn validate_empty() {
+        let mut v = FreestandingVec::new();
+        assert!(v.is_empty());
+
+        for i in 0..3 {
+            v.push(i, || ());
+            assert!(!v.is_empty());
+        }
+
+        for _ in 0..3 {
+            assert!(!v.is_empty());
+            assert!(v.pop(|(), _| ()).is_some());
+        }
+        assert!(v.is_empty());
+    }
+
+    #[test]
+    fn validate_do_nothing() {
+        drop(FreestandingVec::<Vec<usize>, Vec<usize>>::new());
+    }
+
+    #[test]
+    fn validate_type_erased_ops() {
+        let mut v = FreestandingVec::new();
+
+        v.push("42".to_string(), || ());
+
+        let mut v = unsafe { transmute::<_, FreestandingVec<(), ()>>(v) };
+        assert_eq!(
+            Some("42".to_string()),
+            v.pop(|(), ptr| unsafe { ptr.cast::<String>().as_ptr().read() })
+        );
+    }
+
+    #[test]
+    fn validate_raw() {
+        let mut v = FreestandingVec::new();
+        v.push(2048u32, || ());
+
+        let mut v = unsafe { FreestandingVec::from_raw(v.into_raw()) };
+        v.push(1234u32, || ());
+
+        let mut v = unsafe { FreestandingVec::<(), ()>::from_raw(v.into_raw().cast()) };
+        assert_eq!(
+            Some(1234),
+            v.pop(|(), ptr| unsafe { ptr.cast::<u32>().as_ptr().read() })
+        );
+        assert_eq!(
+            Some(2048),
+            v.pop(|(), ptr| unsafe { ptr.cast::<u32>().as_ptr().read() })
+        );
+    }
+
+    #[test]
+    fn zst() {
+        let mut v = FreestandingVec::new();
+
+        for i in 0..1000 {
+            v.push(i, || ());
+        }
+        for i in (0..1000).rev() {
+            assert_eq!(Some(i), v.pop(|(), ptr| unsafe { ptr.as_ptr().read() }));
+        }
+    }
+
+    #[test]
+    fn normal() {
+        let mut v = FreestandingVec::new();
+
+        {
+            let initialized = Rc::<Cell<bool>>::new(Cell::new(false));
+            for i in 0..1000 {
+                v.push(Box::new(i), || {
+                    assert!(!initialized.get());
+                    initialized.set(true);
+
+                    vec![1, 2, 3]
+                });
+            }
+            assert!(initialized.get());
+        }
+
+        for i in (0..1000).rev() {
+            assert_eq!(
+                Some(i),
+                v.pop(|v, ptr| {
+                    assert_eq!(&[1, 2, 3], v.as_slice());
+                    *unsafe { ptr.as_ptr().read() }
+                })
+            );
+        }
+    }
+
+    #[test]
+    fn big() {
+        #[repr(align(512))]
+        #[derive(Clone, Debug)]
+        struct Big(Vec<usize>);
+
+        let mut v = FreestandingVec::new();
+
+        {
+            let initialized = Rc::<Cell<bool>>::new(Cell::new(false));
+            let mut b = Big(Vec::new());
+            for i in 0..100 {
+                b.0.push(i);
+                v.push(b.clone(), || {
+                    assert!(!initialized.get());
+                    initialized.set(true);
+
+                    vec![1, 2, 3]
+                });
+            }
+            assert!(initialized.get());
+        }
+
+        for i in (0..100).rev() {
+            assert_eq!(
+                Some((0..=i).collect::<Vec<_>>()),
+                v.pop(|v, ptr| {
+                    assert_eq!(&[1, 2, 3], v.as_slice());
+                    unsafe { ptr.as_ptr().read() }.0
+                })
+            );
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_in_drop() {
+        #[derive(Debug)]
+        struct P;
+
+        impl Drop for P {
+            fn drop(&mut self) {
+                panic!("Lol get fucked");
+            }
+        }
+
+        let mut v = FreestandingVec::new();
+        v.push((), || P);
+    }
+
+    #[test]
+    #[should_panic]
+    fn panic_in_init() {
+        let mut v = FreestandingVec::new();
+        v.push((), || panic!("Don't even start"));
+    }
+
+    #[test]
+    fn validate_reuse_with_different_type() {
+        fn use_<F: FnOnce() + Clone + 'static>(tasks: &mut FreestandingVec<(), ()>, f: F) {
+            let tasks = unsafe { &mut *ptr::from_mut(tasks).cast::<FreestandingVec<(), F>>() };
+            tasks.push(f, || ());
+            assert!(
+                tasks
+                    .pop(|(), ptr| unsafe { ptr.as_ptr().read() }())
+                    .is_some()
+            );
+        }
+
+        let mut tasks = FreestandingVec::new();
+        use_(&mut tasks, || {
+            dbg!(42);
+        });
+        let v = vec!["a", "b", "c"];
+        use_(&mut tasks, move || {
+            dbg!(v);
+        });
+    }
+}
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,6 +4,9 @@
 #![feature(int_roundings)]
 #![feature(allocator_api)]
 #![feature(cell_update)]
+#![feature(ptr_from_ref)]
+#![feature(strict_provenance)]
 
 pub mod atomic_bag;
 pub mod closure_vec;
+pub mod freestanding_vec;
